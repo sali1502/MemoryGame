ba83093aa558f420723188b044c1238d29b6d55b
sali1502 <sali1502@sudent.miun.se>
2024-11-04 17:25:54 +0100

    Kod för design (knappar, inputfält, topplista och spelfönster).

--- obj/Debug/net8.0-windows/MemoryGame.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/MemoryGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2907dd970141d4391e3592cc240835a77805fb19")]
 [assembly: System.Reflection.AssemblyProductAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MemoryGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
